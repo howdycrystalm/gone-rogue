41da36bd28a3243b4c0278e279bdff29cc90aff7
Crystal Maldonado <cmaldonado@bitwiseindustries.com>
2023-02-19 21:28:43 -0800

    Added ACF to admin. Performance archive page is working. Needs styling

---
+++ archive-performances.php
@@ -0,0 +1,51 @@
+<?php
+/*
+* Template Name: Performances Archive
+*/
+
+get_header(); ?>
+
+<div class="container">
+    <div class="row">
+        <?php
+        $args = array(
+            'post_type' => 'performances',
+            'post_status' => 'publish',
+            'posts_per_page' => -1
+        );
+
+        $query = new WP_Query( $args );
+
+        if ( $query->have_posts() ) :
+            while ( $query->have_posts() ) : $query->the_post(); ?>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'large' ); ?>" class="card-img-top" alt="<?php the_title(); ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php the_title(); ?></h5>
+                            <p class="card-text"><?php the_excerpt(); ?></p>
+                            <!-- <a href="<?php the_permalink(); ?>" class="btn btn-primary"><?php _e( 'Read more', 'textdomain' ); ?></a> -->
+                            <a href="<?php the_permalink(); ?>" class="btn btn-primary"><?php _e( 'Read more', 'textdomain' ); ?></a>
+                            <a href="<?php the_field('buy_ticket_link'); ?>" class="btn btn-primary"><?php _e( 'Buy tickets', 'textdomain' ); ?></a>
+                        </div>
+                        <!-- <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><?php echo get_post_meta( get_the_ID(), 'performance_date', true ); ?></li>
+                        </ul> -->
+                    </div>
+                </div>
+
+            <?php endwhile;
+
+            wp_reset_postdata();
+
+        else :
+
+            echo __( 'No performances found', 'textdomain' );
+
+        endif;
+        ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -17,60 +17,60 @@ add_action( 'wp_enqueue_scripts', 'neve_child_load_css', 20 );
 * Creating a function to create our CPT
 */
   
-function performance_custom_post_type() {
+ function performance_custom_post_type() {
   
-	// Set UI labels for Custom Post Type
-			$labels = array(
-					'name'                => _x( 'Performances', 'Post Type General Name', 'neve' ),
-					'singular_name'       => _x( 'Performance', 'Post Type Singular Name', 'neve' ),
-					'menu_name'           => __( 'Performances', 'neve' ),
-					'parent_item_colon'   => __( 'Parent Performance', 'neve' ),
-					'all_items'           => __( 'All Performances', 'neve' ),
-					'view_item'           => __( 'View Performance', 'neve' ),
-					'add_new_item'        => __( 'Add New Performance', 'neve' ),
-					'add_new'             => __( 'Add New', 'neve' ),
-					'edit_item'           => __( 'Edit Performance', 'neve' ),
-					'update_item'         => __( 'Update Performance', 'neve' ),
-					'search_items'        => __( 'Search Performance', 'neve' ),
-					'not_found'           => __( 'Not Found', 'neve' ),
-					'not_found_in_trash'  => __( 'Not found in Trash', 'neve' ),
-			);
+ 	// Set UI labels for Custom Post Type
+ 			$labels = array(
+ 					'name'                => _x( 'Performances', 'Post Type General Name', 'neve' ),
+ 					'singular_name'       => _x( 'Performance', 'Post Type Singular Name', 'neve' ),
+ 					'menu_name'           => __( 'Performances', 'neve' ),
+ 					'parent_item_colon'   => __( 'Parent Performance', 'neve' ),
+ 					'all_items'           => __( 'All Performances', 'neve' ),
+ 					'view_item'           => __( 'View Performance', 'neve' ),
+ 					'add_new_item'        => __( 'Add New Performance', 'neve' ),
+ 					'add_new'             => __( 'Add New', 'neve' ),
+ 					'edit_item'           => __( 'Edit Performance', 'neve' ),
+ 					'update_item'         => __( 'Update Performance', 'neve' ),
+ 					'search_items'        => __( 'Search Performance', 'neve' ),
+ 					'not_found'           => __( 'Not Found', 'neve' ),
+ 					'not_found_in_trash'  => __( 'Not found in Trash', 'neve' ),
+ 			);
 				
-	// Set other options for Custom Post Type
+ 	// Set other options for Custom Post Type
 				
-			$args = array(
-					'label'               => __( 'performances', 'neve' ),
-					'description'         => __( 'Performance news and reviews', 'neve' ),
-					'labels'              => $labels,
-					// Features this CPT supports in Post Editor
-					'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
-					// You can associate this CPT with a taxonomy or custom taxonomy. 
-					'taxonomies'          => array( 'performance', 'performances' ),
-					/* A hierarchical CPT is like Pages and can have
-					* Parent and child items. A non-hierarchical CPT
-					* is like Posts.
-					*/
-					'hierarchical'        => false,
-					'public'              => true,
-					'show_ui'             => true,
-					'show_in_menu'        => true,
-					'show_in_nav_menus'   => true,
-					'show_in_admin_bar'   => true,
-					'menu_position'       => 5,
-					'menu_icon'						=> "dashicons-microphone",
-					'can_export'          => true,
-					'has_archive'         => true,
-					'exclude_from_search' => false,
-					'publicly_queryable'  => true,
-					'capability_type'     => 'post',
-					'show_in_rest' => true,
+ 			$args = array(
+ 					'label'               => __( 'performances', 'neve' ),
+ 					'description'         => __( 'Performance news and reviews', 'neve' ),
+ 					'labels'              => $labels,
+ 					// Features this CPT supports in Post Editor
+ 					'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+ 					// You can associate this CPT with a taxonomy or custom taxonomy. 
+ 					'taxonomies'          => array( 'performance', 'performances' ),
+ 					/* A hierarchical CPT is like Pages and can have
+ 					* Parent and child items. A non-hierarchical CPT
+ 					* is like Posts.
+ 					*/
+ 					'hierarchical'        => false,
+ 					'public'              => true,
+ 					'show_ui'             => true,
+ 					'show_in_menu'        => true,
+ 					'show_in_nav_menus'   => true,
+ 					'show_in_admin_bar'   => true,
+ 					'menu_position'       => 5,
+ 					'menu_icon'						=> "dashicons-microphone",
+ 					'can_export'          => true,
+ 					'has_archive'         => true,
+ 					'exclude_from_search' => false,
+ 					'publicly_queryable'  => true,
+ 					'capability_type'     => 'post',
+ 					'show_in_rest' => true,
 		
-			);
+ 			);
 				
-			// Registering your Custom Post Type
-			register_post_type( 'performances', $args );
+ 			// Registering your Custom Post Type
+ 			register_post_type( 'performances', $args );
 		
-	}
+ 	}
 		
 	/* Hook into the 'init' action so that the function
 	* Containing our post type registration is not 
@@ -80,5 +80,4 @@ function performance_custom_post_type() {
 	add_action( 'init', 'performance_custom_post_type', 0 );
 
 //archives-page-functions.php add widgets and enqueues archives-page-style.css
-
-// require get_theme_file_path('/archives-page-functions.php');
+require get_theme_file_path('/views/archives-page-functions.php');
